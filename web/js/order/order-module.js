b6edbde39b0ab7c4d9a43c61ff2d0abdf4fafd52
Francis Poirier <fpoirier@gmail.com>
2014-01-23 10:29:51 -0500

    Proposal and Order placeholder

--- web/js/order/order-module.js
+++ web/js/order/order-module.js
@@ -1,113 +1,143 @@
-var module = angular.module("Order", ['ui.sortable', 'tc.directives.numbers','textAngular']);
+var module = angular.module("Order", ['ui.sortable', 'tc.directives.numbers', 'textAngular']);
 
-module.controller("order.Controller", function($scope, form_name, order){    
-    
+module.controller("order.Controller", function($scope, form_name, OrderGuide, order){
+
     $scope.form_name = form_name;
-    
+
     function getFormName(prop){
-        return form_name+"["+prop+"]";
-    };
-    
+        return form_name + "[" + prop + "]";
+    }
+    ;
+
     $scope.getFormName = function(prop){
         return getFormName(prop);
     };
-    
-    /* **************************************** */    
+
+    /* **************************************** */
     /* ******        OFFER BLOCKS        ****** */
     /* **************************************** */
-    
-    function getBlockIndex( block ){
+
+    function getBlockIndex(block){
         return order.offer.indexOf(block);
-    };
-    
+    }
+    ;
+
     $scope.getOfferFormName = function(block, prop){
         var i = getBlockIndex(block);
-        return getFormName('offer')+"["+i+"]["+prop+"]";
+        return getFormName('offer') + "[" + i + "][" + prop + "]";
     };
-    
+
     $scope.addBlock = function(){
         //Add the new one to the list
-        $scope.order.offer.push({'title':'', 'body':''});
-    
+        $scope.order.offer.push({ 'title' : '', 'body' : '' });
+
         $scope.offerForm.$setDirty();
     };
-    
+
     $scope.removeBlock = function(block){
         var i = getBlockIndex(block);
-        $scope.order.offer.splice(i,1);
+        $scope.order.offer.splice(i, 1);
+        
+        if ( $scope.offerIsEmpty() )
+            $scope.order.offer.push({ 'title' : '', 'body' : '' });
+        
         $scope.offerForm.$setDirty();
     }
-    
+
     $scope.offerIsEmpty = function(){
-        return order.offer.length<=0;
+        return order.offer.length <= 0;
     }
-    
+
     $scope.order = order;
-    
+
     $scope.$watch("order.offer", function(newValue, oldValue){
-        if($scope.offerIsEmpty())
-            order.offer.push({'title':'', 'body':''})
+        if ( $scope.offerIsEmpty() )
+            OrderGuide.fill(order);
     });
-    
+
     $scope.sortableOptions = {
-        update: function() { 
+        update : function(){
             $scope.offerForm.$setDirty();
         },
-        helper: function(e, elem){
+        helper : function(e, elem){
             return $(elem).clone().appendTo("body");
-        }, 
-        placeholder: "tc-placeholder-block",        
-        handle: ".tc-drag-block"
+        },
+        placeholder : "tc-placeholder-block",
+        handle : ".tc-drag-block"
     };
-        
-    /* **************************************** */    
+
+    /* **************************************** */
     /* ******        DELIVERABLES        ****** */
     /* **************************************** */
-    
-    function getDeliverableIndex( deliverable ){
+
+    function getDeliverableIndex(deliverable){
         return order.deliverables.indexOf(deliverable);
-    };
-    
+    }
+    ;
+
     $scope.getDeliverableFormName = function(deliverable, prop){
         var i = getDeliverableIndex(deliverable);
-        return getFormName('deliverables')+"["+i+"]["+prop+"]";
+        return getFormName('deliverables') + "[" + i + "][" + prop + "]";
     };
-    
+
     $scope.addDeliverable = function(){
         //Add the new one to the list
         $scope.order.deliverables.push($scope.newDeliverable);
-        
+
         //Resets newDeliverable
-        $scope.newDeliverable = {name:'', 'cost':null, 'quantity':1};
-    
+        $scope.newDeliverable = { name : '', 'cost' : null, 'quantity' : 1 };
+
         $scope.deliverablesForm.$setDirty();
     };
-    
+
     $scope.removeDeliverable = function(deliverable){
         var i = getDeliverableIndex(deliverable);
-        $scope.order.deliverables.splice(i,1);
+        $scope.order.deliverables.splice(i, 1);
         $scope.deliverablesForm.$setDirty();
     }
-    
+
     $scope.deliverablesIsEmpty = function(){
-        return order.deliverables.length<=0;
+        return order.deliverables.length <= 0;
     }
-    
+
     // Store new deliverables 
-    $scope.newDeliverable = {name:'', 'cost':null, 'quantity':1};
+    $scope.newDeliverable = { name : '', 'cost' : null, 'quantity' : 1 };
     /* *************************************** */
-    
+
     $scope.getTotal = function(){
         var deliverable;
         var total = 0;
-        for( var i in $scope.order.deliverables ){
+        for ( var i in $scope.order.deliverables ) {
             deliverable = $scope.order.deliverables[i];
             total += deliverable.quantity * deliverable.cost;
         }
         return total;
     }
-    
-    
-    
-    
+
+});
+
+module.factory('OrderGuide', function($sanitize){
+
+    function fill(order){
+        if ( !order )
+            return;
+
+        order.offer = [
+            {
+                'title' : 'Project Description',
+                'body' : $sanitize('The mandate consist of <strong>[updating, creating, etc.]</strong> over a period of <strong>[delay]</strong>.')
+            },
+            
+            {
+                'title' : 'Work Specifications',
+                'body' : $sanitize('Here is the list of specifications required for the <strong>[project name]</strong> : <ul><li><strong>[Every images will be created using Adobe Photoshop Version 6.0]</strong></li><li><strong>[Website will be coded using PHP]</strong></li><li><strong>[2 Rounds of reviews will be allowed]</strong></li></ul>')
+            }
+        ];
+    }
+
+    // Return Public API
+    return {
+        fill : fill
+    };
+
 });
--- web/js/rfp/rfp-module.js
+++ web/js/rfp/rfp-module.js
@@ -1,6 +1,6 @@
 var module = angular.module("RFP", ['ui.sortable','textAngular']);
 
-module.controller("rfp.Controller", function($scope, form_name, rfp){
+module.controller("rfp.Controller", function($scope, form_name, RFPGuide, rfp){
         
     function getFormName(prop){
         return form_name+"["+prop+"]";
@@ -33,6 +33,10 @@ module.controller("rfp.Controller", function($scope, form_name, rfp){
     $scope.removeBlock = function(block){
         var i = getBlockIndex(block);
         $scope.rfp.body.splice(i,1);
+        
+        if ( $scope.isRFPEmpty() )
+            $scope.rfp.body.push({ 'title' : '', 'body' : '' });
+        
         $scope.rfpForm.$setDirty();
     }
     
@@ -46,7 +50,7 @@ module.controller("rfp.Controller", function($scope, form_name, rfp){
     
     $scope.$watch("rfp.body", function(newValue, oldValue){
         if($scope.isRFPEmpty())
-            rfp.body.push({'title':'', 'body':''})
+            RFPGuide.fill(rfp);
     });
     
     $scope.sortableOptions = {
@@ -59,4 +63,45 @@ module.controller("rfp.Controller", function($scope, form_name, rfp){
         placeholder: "tc-placeholder-block",
         handle: ".tc-drag-block"
     };
+});
+
+module.factory('RFPGuide', function($sanitize){
+
+    function fill(rfp){
+        if ( !rfp )
+            return;
+
+        rfp.body = [
+            {
+                'title' : 'Introduction',
+                'body' : $sanitize('<strong>[ABC Inc.]</strong> is seeking the services of <strong>[an illustrator, a Web Designer, etc.]</strong> on a contract basis to produce a variety of <strong>[illustrative material, designs, etc.]</strong> for the <strong>[project 123]</strong>.')
+            },
+            
+            {
+                'title' : '[Illustrator, Web Design, etc.]',
+                'body' : $sanitize('The required <strong>[illustrator, Web Designer, etc.]</strong> will need to be able to <strong>[develop illustrative materials (pixel or vector) for an e-Learning program]</strong>, using <strong>[Adobe Photoshop]</strong>.')
+            },
+            
+            {
+                'title' : 'Work Requirements',
+                'body' : $sanitize('Ensure a consistent brand identity of <strong>[our company, a previous project, the following portfolio: www.ourcompany.com/portfolio]</strong>.')
+            },
+            
+            {
+                'title' : 'Expectations',
+                'body' : $sanitize('<strong>[ABC Inc.]</strong> is requesting a quote including estimated costs and delivery dates in <strong>[January 2014]</strong> for the following: <ul><li><strong>[Requirement 1 – Description of the requirement]</strong>.</li><li><strong>[Requirement 2 – Description of the requirement]</strong>.</li><li><strong>[Requirement 3 – Description of the requirement]</strong>.</li></ul>')
+            },
+            
+            {
+                'title' : 'Estimated Project Duration',
+                'body' : $sanitize('<strong>[ABC Inc.]</strong> required all <strong>[illustrative material]</strong> to be completed <strong>[before January 23th 2014]</strong>.')
+            }
+        ];
+    }
+
+    // Return Public API
+    return {
+        fill : fill
+    };
+
 });
