075da78376f47b616636afac30ccfc199efb5ede
Francis Poirier <fpoirier@gmail.com>
2014-02-13 12:01:13 -0500

    EditProposalCommand.php

---
+++ src/TC/CoreBundle/Cqrs/EditProposalCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EditProposalCommand
+ *
+ * @author Francis Poirier
+ */
+class EditProposalCommand {
+    //put your code here
+}
+
+?>
